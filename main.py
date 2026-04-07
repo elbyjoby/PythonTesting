97d6120d4af01c71878983963446bfc6260b81a2
elbyjoby <elbyjoby18@gmail.com>
2026-04-07 23:38:04 +0100

    changed again

--- main.py
+++ main.py
@@ -29,9 +29,6 @@ rbiObject.credit(100)
 rbiObject.get_balance()
 
 rbiObject = HdfcBankaccount(250)
-rbiObject.debit(500)
-rbiObject.debit(150)
-rbiObject.get_balance()
 rbiObject.credit(100)
 rbiObject.get_balance()
 
